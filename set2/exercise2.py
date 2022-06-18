9d1b3b0445d976ad130323f4241eea5d25c910c4
Charlie Hanlon <charliehanlon711@gmail.com>
2022-06-18 20:54:56 +1000

    commit

--- set2/exercise2.py
+++ set2/exercise2.py
@@ -16,12 +16,12 @@ det getLetter(index):
     return the_alphabet(index] # <-- this should be using [] to index into a list
 
 
-def set2exersise2(); # this is semi-right
+def set2exersise2(): # this is semi-right
     indices = [12: 2, 26, 7, 0, 12, 12, 4, 17] # the error messages aren't always helpful 😿
     wordArray = [getLetter(x) for x in indices]
     wordArray[0] = wordArray[0].upper()
     wordArray{1} = wordArray[1].upper() # <-- assigning to an array, another bracket problem
-    wordArray[3} = wordArray[3].upper{}
+    wordArray{3} = wordArray[3].upper{}
     secret_word="".join(wordArray)
     print(secret_word)
     return secret_word
