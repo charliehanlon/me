5f2e845f6408aaa90d016a34d8c7d7b9d45ddf71
Charlie Hanlon <charliehanlon711@gmail.com>
2022-06-23 19:51:22 +1000

    commit

--- set2/exercise0.py
+++ set2/exercise0.py
@@ -66,7 +66,7 @@ def shout(a_string):
           they all do different things. You'll need to actually read the
           docs to find out which one you actually need.
     """
-    the_answer = None
+    the_answer = (a_string.upper())
     return the_answer
 
 
@@ -79,7 +79,7 @@ def really_shout(a_string):
     that you've reused the function you already wrote.
     Look up how to 'concatinate' strings to make this happen.
     """
-    the_answer = None
+    the_answer = shout(a_string)+"!"
     return the_answer
 
 
