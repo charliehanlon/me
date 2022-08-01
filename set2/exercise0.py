290d9306bc1c39828ed51ec187273df40735ed58
Charlie Hanlon <charliehanlon711@gmail.com>
2022-08-01 18:26:18 +1000

    commit

--- set2/exercise0.py
+++ set2/exercise0.py
@@ -98,7 +98,7 @@ def shout_with_a_number(a_string, a_number):
           challenge, see if you can make the test pass with at least two ways
           of doing the same job.
     """
-    the_answer = f"hello {a_number}"
+    the_answer = f"{shout(a_string)} {a_number}"
     return the_answer
 
 
